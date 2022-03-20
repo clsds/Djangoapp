3d27d3ccf7b083bfa597d1b73e52f19bce8b28bb
输clsds <1353080085@qq.com>
2022-03-20 23:00:53 +0800

    modify allowed host

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ok$#+8h#n*ypd06*#pacr_@buyx23y)4_8&6r%lw75$5x!c^a-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["120.77.233.153"]
 
 
 # Application definition
